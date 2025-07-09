848cf38bf70b8f131d13b6fb18273bc3783537eb
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-07-08 21:36:38 -0300

    Comitando apos teste do api

---
+++ script.js
@@ -0,0 +1,14 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Njk5MzQ5ZTNlM2RiMDQzZjY5MzljZWM0ZDNhZTI0NyIsIm5iZiI6MTc1MjAxODc3MS4zODIwMDAyLCJzdWIiOiI2ODZkYWY1M2RmMTFkZjY1ZmE1NDBiNjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.OgTFhhLDlVvZa-9DeP7C4HQc7Ll_C_i9DZWCydV3rwo'
+    }
+  };
+
+fetch('https://api.themoviedb.org/3/authentication')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+
