84799d3f0b2dcb89e8a2eee9686f1c46cdeb5fe0
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-07-15 21:24:14 -0300

    comitando após colocar card, css e funcoes

--- script.js
+++ script.js
@@ -6,13 +6,46 @@ const options = {
     }
   };
 
-// fetch('https://api.themoviedb.org/3/authentication')
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data);
-// })
 
 fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-.then(res => res.json())
-.then(res => console.log(res))
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  LoadCardMovies(data.results)
+})
 .catch(err => console.error(err));
+
+const LoadCardMovies = (filmes) => {
+const container = document.getElementById('movie-container');
+
+  
+ filmes.forEach(filme => {
+
+  const divCol = document.createElement('div')
+  divCol.className = 'col-12 col-sm-6 col-md-4 col-lg-3 mb-4'
+
+const divCard = document.createElement('div');
+divCard.className = 'card h-100 d-flex flex-column'
+
+const imageURL = 'https://media.themoviedb.org/t/p/w440_and_h660_face/'
+divCard.innerHTML = 
+` <img src="${imageURL}${filme.poster_path}" class="card-img-top" alt="...">
+  <div class="card-body d-flex flex-column">
+    <h5 class="card-title">${filme.title}</h5>
+    <p class="card-text">${filme.overview}</p>
+    <div class = "mt-auto d-flex justify-content-between align-items-center"> 
+    <a href="#" class="btn btn-primary">Ver Mais</a>
+    <small class= "text-muted"><strong>Estreia: </strong>${formatDate(filme.release_date)}</small>
+    </div>
+   </div>`
+
+  divCol.appendChild(divCard);
+  container.appendChild(divCol);
+ });  
+
+}
+
+const formatDate = (data) => {
+  const [ano, mes, dia] = data.split('-')
+  return `${dia}/${mes}/${ano}`
+}
